d1bc0cfa30104eb3824af7641d86af4ab324317c
James Ahlm <14782828428@qq.com>
2016-12-28 12:43:03 +0800

    Fixed filenames

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var routes = require('./app/routes/index.js')
-
 var app = express()
+
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-routes(app)
+routes(app);
 
 
 app.listen(process.env.PORT || 5000);
